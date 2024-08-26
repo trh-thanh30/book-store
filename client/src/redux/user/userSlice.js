d56aac43cbcf553c15910f4e6be85228c3bc7295
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-26 16:53:28 +0700

    create set up redux-tolkit user

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,70 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  currentUser: null,
+  error: null,
+  loading: false,
+};
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    SignInStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    SignInSuccess: (state, action) => {
+      state.loading = false;
+      state.currentUser = action.payload;
+      state.error = null;
+    },
+    SignInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    signOut: (state) => {
+      state.currentUser = null;
+      state.error = null;
+      state.loading = false;
+    },
+    updateUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateUserSuccess: (state, action) => {
+      state.loading = false;
+      state.currentUser = action.payload;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+      state.currentUser = null;
+    },
+    deleteUserSuccess: (state) => {
+      state.loading = false;
+      state.error = null;
+      state.currentUser = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
+export const {
+  SignInStart,
+  SignInSuccess,
+  SignInFailure,
+  signOut,
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+} = userSlice.actions;
+export default userSlice.reducer;
